3669c42b4a20ffbd99095c462929cfa40197f82d
James Osborn <jamescarlosborn@gmail.com>
2017-10-01 22:33:22 -0700

    fix back end logic, rename variables to make them more distincton front and back end, make pingPong a usable function on the front end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
   // Business logic
-var pingPong = function(playerNumber) {
+var pingPong = function(numbers) {
   var userArray = [];
-  for (var index = 1; index <= playerNumber; index += 1) {
-    alert(index);
+  for (var index = 1; index <= numbers; index += 1) {
+    // alert(index);
      if (index % 15 === 0) {
      userArray.push("pingPong")
-     console.log(userArray)
+   } else if (index % 11 === 0) {
+     userArray.push("Match!")
    } else if (index % 5 === 0) {
      userArray.push("pong")
    } else if (index % 3 === 0) {
      userArray.push("ping")
+   } else if (index % 50 === 0) {
+     userArray.push("Champion")
+   } else if (index % 100 === 0) {
+     userArray.push("Legendary")
    } else {
-     userArray.push();
-   } return userArray;
-  }
+     userArray.push(index);
+     console.log(userArray);
+   }
+  } return userArray;
 };
 
   // UI Logic
@@ -21,11 +27,11 @@ var pingPong = function(playerNumber) {
 $(document).ready(function() {
   $("form#pingPongForm").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("input#playerNumber").val());
-    var result = pingPong(playerNumber);
-    // result.forEach(function(rally) {
-    //   $("#result").append("<li>" + rally + "</li>");
-    // });
+    var userInput = parseInt($("input#playerNumber").val());
+    var result = pingPong(userInput);
+    result.forEach(function(rally) {
+    $("#result").append("<li>" + result + "</li>");
+    });
 
   });
 });
