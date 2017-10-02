b662b83361f4a48effaa9f96d53e3c46ac2a0d8a
James Osborn <jamescarlosborn@gmail.com>
2017-10-01 23:47:26 -0700

    refine back end logic, add conditions for Match, Champion and Legendary.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
   // Business logic
 var pingPong = function(numbers) {
   var userArray = [];
-  for (var index = 1; index <= numbers; index += 1) {
+  for (var index = 1; index <= numbers; index++) {
     // alert(index);
-     if (index % 15 === 0) {
+     if (index % 100 === 0) {
+     userArray.push("Legendary")
+   } else if (index % 50 === 0) {
+     userArray.push("Champion")
+   } else if (index % 15 === 0) {
      userArray.push("pingPong")
    } else if (index % 11 === 0) {
      userArray.push("Match!")
@@ -11,27 +15,18 @@ var pingPong = function(numbers) {
      userArray.push("pong")
    } else if (index % 3 === 0) {
      userArray.push("ping")
-   } else if (index % 50 === 0) {
-     userArray.push("Champion")
-   } else if (index % 100 === 0) {
-     userArray.push("Legendary")
    } else {
      userArray.push(index);
      console.log(userArray);
    }
-  } return userArray;
+   } return userArray;
 };
-
   // UI Logic
-
 $(document).ready(function() {
   $("form#pingPongForm").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#playerNumber").val());
     var result = pingPong(userInput);
-    result.forEach(function(rally) {
-    $("#result").append("<li>" + result + "</li>");
-    });
-
+    $("#result").text(result);
   });
 });
